be85789d72ee99dcfc3ca7e393fa19a9f1c46f3b
Vicky Pranandika Wijaksana 1915016185 <62289297+VickyPranandikaWijaksana1915016185@users.noreply.github.com>
2022-06-19 20:11:04 +0800

    Add files via upload

---
+++ toko_alat_musik_app/lib/keranjangpage.dart
@@ -0,0 +1,81 @@
+import 'package:flutter/material.dart';
+import 'package:toko_alat_musik_app/Item_Card_Keranjang.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+class KeranjangPage extends StatefulWidget {
+  const KeranjangPage({Key? key}) : super(key: key);
+
+  @override
+  State<KeranjangPage> createState() => _KeranjangPageState();
+}
+
+class _KeranjangPageState extends State<KeranjangPage> {
+  @override
+  Widget build(BuildContext context) {
+    FirebaseAuth auth = FirebaseAuth.instance;
+    String uid = auth.currentUser!.uid.toString();
+    FirebaseFirestore firestore = FirebaseFirestore.instance;
+    CollectionReference keranjang = firestore.collection("Keranjang");
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Keranjang',
+          style: TextStyle(color: Colors.white),
+        ),
+        automaticallyImplyLeading: false,
+      ),
+      backgroundColor: Colors.white,
+      body: Stack(children: [
+        Column(crossAxisAlignment: CrossAxisAlignment.start, children: <Widget>[
+          Expanded(
+            //// VIEW DATA HERE
+            child: StreamBuilder<QuerySnapshot>(
+              stream: keranjang.where('Id', isEqualTo: uid).snapshots(),
+              builder: (_, AsyncSnapshot<QuerySnapshot> snapshot) {
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return Text("Loading");
+                }
+                if (snapshot.hasData) {
+                  return GridView.count(
+                      crossAxisCount: 1,
+                      childAspectRatio: 3,
+                      children: snapshot.data!.docs
+                          .map(
+                            (e) => ItemCardKeranjang(
+                                e['Image'],
+                                e['Barang'],
+                                e['Harga'],
+                                e['Jumlah'],
+                                e['Deskripsi'],
+                                e['Jenis'], onUpdate: () {
+                              keranjang.doc(e.id).update(
+                                {
+                                  'Jumlah': e.get('Jumlah') + 1,
+                                },
+                              );
+                            }, onUpdate2: () {
+                              if (e.get('Jumlah') <= 1) {
+                              } else {
+                                keranjang.doc(e.id).update(
+                                  {
+                                    'Jumlah': e.get('Jumlah') - 1,
+                                  },
+                                );
+                              }
+                            }, onDelete: () {
+                              keranjang.doc(e.id).delete();
+                            }),
+                          )
+                          .toList());
+                } else {
+                  return Text("Barang Tidak Ada");
+                }
+              },
+            ),
+          ),
+        ]),
+      ]),
+    );
+  }
+}
